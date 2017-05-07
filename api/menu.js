5c1bfa6dd500876a6517769b42a0e10d29e94398
Teasue <296609391@qq.com>
2017-05-07 12:48:42 +0800

    menu&table

--- api/menu.js
+++ api/menu.js
@@ -1,26 +1,175 @@
 var menu_model = require('../proxy/menu');
+var table_model = require('../proxy/table');
+var uuidV1 = require('uuid/v1');
 
 /**
  * 用于获取菜单的类别，不添加权限，对所有类别开放
  */
-exports.type = function*(next){
-    try{
+exports.type = function* (next) {
+    try {
         var type = yield menu_model.type();
-        this.body = {success:true,data:type};
-    }catch(e){
-        this.body = {success:false,data:e};
+        this.body = { success: true, data: type };
+    } catch (e) {
+        this.body = { success: false, data: e };
     }
 }
 
 /**
  * 获取某一类菜下面的所有的菜品
  */
-exports.oneType = function*(next){
+exports.oneType = function* (next) {
     var id = this.params.id;
-    try{
+    try {
         var dishs = yield menu_model.oneType(id);
-        this.body = {success:true,data:dishs};
-    }catch(e){
-        this.body = {success:false,data:e};
+        this.body = { success: true, data: dishs };
+    } catch (e) {
+        this.body = { success: false, data: e };
+    }
+}
+/**
+ * 查看一道菜的具体信息
+ */
+exports.oneDish = function* (next) {
+    var id = this.params.id;
+    try {
+        var dish = yield menu_model.oneDish(id);
+        this.body = { success: true, data: dish };
+    } catch (e) {
+        this.body = { success: false, data: e };
+    }
+}
+/**
+ * 新增一种菜单类别
+ */
+exports.addType = function* (next) {
+    var userID = '6';
+    var auth = yield table_model.getAuth(userID);
+    var auth = auth[0].Auth;
+    if (auth == '7') {
+        var { ClassName, ClassDescription } = this.request.body;
+        var info = yield menu_model.addType({
+            ID: uuidV1(),
+            ClassName: ClassName,
+            ClassDescription: ClassDescription
+        });
+        this.bofy = { success: true, data: info };
+    }
+    else {
+        this.body = { success: false };
+    }
+}
+/**
+ * 删除一种菜单类别
+ */
+exports.deleteType = function* (next) {
+    var userID = '6';
+    var auth = yield table_model.getAuth(userID);
+    var auth = auth[0].Auth;
+    if (auth == '7') {
+        var { ID } = this.request.body;
+        var info = yield menu_model.deleteType(ID);
+        this.bofy = { success: true, data: info };
+    }
+    else {
+        this.body = { success: false };
+    }
+}
+/**
+ * 修改一种菜单类别
+ */
+exports.updateType = function* (next) {
+    var userID = '6';
+    var auth = yield table_model.getAuth(userID);
+    var auth = auth[0].Auth;
+    if (auth == '7') {
+        var { ID, ClassName, ClassDescription } = this.request.body;
+        var info = yield menu_model.updateType({
+            ID: ID,
+            ClassName: ClassName,
+            ClassDescription: ClassDescription
+        });
+        this.bofy = { success: true, data: info };
+    }
+    else {
+        this.body = { success: false };
+    }
+}
+/**
+ * 新增一道菜
+ */
+exports.addDish = function* (next) {
+    var userID = '6';
+    var auth = yield table_model.getAuth(userID);
+    var auth = auth[0].Auth;
+    if (auth == '7') {
+        var { Description, ClassID, Price, Name } = this.request.body;
+        var info = yield menu_model.addDish({
+            ID: uuidV1(),
+            Description: Description,
+            ClassID: ClassID,
+            Price: Price,
+            Name: Name,
+            Status: 'Available'
+        });
+        this.bofy = { success: true, data: info };
+    }
+    else {
+        this.body = { success: false };
+    }
+}
+/**
+ * 删除一道菜
+ */
+exports.deleteDish = function* (next) {
+    var userID = '6';
+    var auth = yield table_model.getAuth(userID);
+    var auth = auth[0].Auth;
+    if (auth == '7') {
+        var { ID } = this.request.body;
+        var info = yield menu_model.deleteDish(ID);
+        console.log(info);
+        this.bofy = { success: true, data: info };
+    }
+    else {
+        this.body = { success: false };
+    }
+}
+/**
+ * 修改一道菜
+ */
+exports.updateDish = function* (next) {
+    var userID = '6';
+    var auth = yield table_model.getAuth(userID);
+    var auth = auth[0].Auth;
+    if (auth == '7') {
+        var { ID, Description, ClassID, Price, Name } = this.request.body;
+        var info = yield menu_model.updateDish({
+            ID: ID,
+            Description: Description,
+            ClassID: ClassID,
+            Price: Price,
+            Name: Name,
+            Status: 'Available'
+        });
+        this.bofy = { success: true, data: info };
+    }
+    else {
+        this.body = { success: false };
+    }
+}
+/**
+ * 暂停一道菜的制作
+ */
+exports.stopDish = function* (next) {
+    var userID = '6';
+    var auth = yield table_model.getAuth(userID);
+    var auth = auth[0].Auth;
+    if (auth == '7') {
+        var { ID } = this.request.body;
+        var info = yield menu_model.stopDish(ID);
+        this.body = { success: true, data: info };
+    }
+    else {
+        this.body = { success: false };
     }
 }
--- api_router.js
+++ api_router.js
@@ -1,32 +1,48 @@
 var router = require('koa-router')();
 var user = require('./api/user');
 var menu = require('./api/menu');
+var table = require('./api/table');
 
 router.prefix('/api/v1');
 
-router.use(function*(next){
-    var {AccessToken} = this.request.body;
-    if(!AccessToken){
-        
+router.use(function* (next) {
+    var { AccessToken } = this.request.body;
+    if (!AccessToken) {
+
     }
     yield next;
 });
 
-router.post('/signin',user.signin);//登陆
-router.post('/signout',user.signout);//登出
-router.post('/signup',user.signup);//注册
+router.post('/signin', user.signin);//登陆
+router.post('/signout', user.signout);//登出
+router.post('/signup', user.signup);//注册
 
 /**
  * user部分
  */
-router.get('/user/:id',user.get);
+router.get('/user/:id', user.get);
 
 
 /**
  * 菜单部分
  */
-router.get('/menu/type/:id',menu.oneType);
-router.get('/menu/type',menu.type);
+router.post('/menu/type/add', menu.addType);
+router.post('/menu/type/delete', menu.deleteType);
+router.post('/menu/type/update', menu.updateType);
+router.get('/menu/type/:id', menu.oneType);
+router.get('/menu/type', menu.type);
+router.post('/menu/dish/add', menu.addDish);
+router.post('/menu/dish/delete', menu.deleteDish);
+router.post('/menu/dish/update', menu.updateDish);
+router.post('/menu/dish/stop', menu.stopDish);
+router.get('/menu/dish/:id', menu.oneDish);
 
+/**
+ * table部分
+ */
+router.post('/table/:id/bind', table.bind);
+router.get('/table', table.table);
+router.get('/table/:id', table.oneTable);
+router.post('/table/cleanup', table.cleanup);
 
 module.exports = router;
