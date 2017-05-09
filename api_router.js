193906dadb37c1e2348aa91e86a028c217d14b7f
Teasue <296609391@qq.com>
2017-05-09 14:38:09 +0800

    添加了api的权限控制

--- api_router.js
+++ api_router.js
@@ -1,4 +1,3 @@
-
 const router = require('koa-router')();
 const user = require('./api/user');
 const menu = require('./api/menu');
@@ -10,37 +9,19 @@ const token = require('./middleware/token.js');// token转换函数
 
 //  权限控制函数
 const authRequired = authControl.authRequired;
-
-
-router.use(function* (next) {
-    var { AccessToken } = this.request.body;
-    if (!AccessToken) {
-
-    }
-    yield next;
-});
-
-
-
-
-
 router.prefix('/api/v1');
-router.use(token.token2session);//  将传入的Token值查找到用户之后转换为session
 
-router.post('/signin', sign.signin);// 登陆
-router.get('/signout', sign.signout);// 登出
-router.post('/signup', sign.signup);// 注册
-router.post('/employeeSignin', sign.employeeSignin);// 员工登陆
-
+router.post('/signin', user.signin);//登陆
+router.post('/signout', user.signout);//登出
+router.post('/signup', user.signup);//注册
 /**
  * order部分
  */
-router.post('/order/reserve', order.reserve);// 预定
-router.post('/order/:id/add', order.addDish);// 增加要做的菜
-router.post('/order/:id/sub', order.subDish);// 删除某道未做的菜
-router.post('/order/:id/cancel', order.cancelOrder);// 删除某道未做的菜
-router.post('/order/:id/pay', order.payforOrder);// 删除某道未做的菜
-
+router.post('/order/reserve',order.reserve);//预定
+router.post('/order/:id/add',order.addDish);//增加要做的菜
+router.post('/order/:id/sub',order.subDish);//删除某道未做的菜
+router.post('/order/:id/cancel',order.cancelOrder);//删除某道未做的菜
+router.post('/order/:id/pay',order.payforOrder);//删除某道未做的菜
 
 /**
  * user部分
@@ -49,30 +30,25 @@ router.get('/user/:id', user.get);//  获取用户的基本信息
 router.get('/user/canOrder', user.canOrder);// 判断自己是否可以开始点餐了
 router.get('/user', user.getOwnInfo);//   获取自己的基本信息
 
-
-
-
 /**
  * 菜单部分
  */
-router.post('/menu/type/add', menu.addType);
-router.post('/menu/type/delete', menu.deleteType);
-router.post('/menu/type/update', menu.updateType);
-router.get('/menu/type/:id', menu.oneType);
-router.get('/menu/type', menu.type);
-router.post('/menu/dish/add', menu.addDish);
-router.post('/menu/dish/delete', menu.deleteDish);
-router.post('/menu/dish/update', menu.updateDish);
-router.post('/menu/dish/stop', menu.stopDish);
-router.get('/menu/dish/:id', menu.oneDish);
-
-
+router.post('/menu/type/add',authRequired(7), menu.addType);
+router.post('/menu/type/delete',authRequired(7) ,menu.deleteType);
+router.post('/menu/type/update',authRequired(7), menu.updateType);
+router.get('/menu/type/:id',authRequired(5), menu.oneType);
+router.get('/menu/type',authRequired(5), menu.type);
+router.post('/menu/dish/add',authRequired(7), menu.addDish);
+router.post('/menu/dish/delete', authRequired(7),menu.deleteDish);
+router.post('/menu/dish/update', authRequired(7),menu.updateDish);
+router.post('/menu/dish/stop', authRequired(7),menu.stopDish);
+router.get('/menu/dish/:id',authRequired(5), menu.oneDish);
 /**
- * 餐桌部分
+ * table部分
  */
-router.post('/table/:id/bind', authRequired(3), table.bind);
-router.get('/table', table.table);
-router.get('/table/:id', table.oneTable);
-router.post('/table/cleanup', table.cleanup);
+router.post('/table/:id/bind', authRequired(3) ,table.bind);
+router.get('/table',authRequired(6), table.table);
+router.get('/table/:id',authRequired(3) ,table.oneTable);
+router.post('/table/cleanup',authRequired(6), table.cleanup);
 
 module.exports = router;
