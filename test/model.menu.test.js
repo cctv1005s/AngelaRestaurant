c5ba2befff5f152ecb5a36d4c5f24176eb6894bd
GorgearYang <384113872@qq.com>
2017-05-17 11:46:44 +0800

    提交了用户登陆的测试代码

--- test/model.menu.test.js
+++ test/model.menu.test.js
@@ -1,24 +1,15 @@
 const menuModel = require('../proxy/menu.js');
 const co = require('co');
 const expect = require('chai').expect;
+require('co-mocha');
 
 describe('菜单数据库测试', () => {
-  it('获取类别测试', (done) => {
-    co(function* () {
+  it('获取类别测试', function*(){
       const r = yield menuModel.type();
       expect(r).to.be.a('array');
-      done();
-    }).catch((e) => {
-      throw new Error(e);
-    });
   });
 
-  it('获取某一个类别下的菜的测试', (done) => {
-    co(function* () {
+  it('获取某一个类别下的菜的测试', function*(){
       const r = yield menuModel.oneType('1');
-      done();
-    }).catch((e) => {
-      throw new Error(e);
-    });
   });
 });
